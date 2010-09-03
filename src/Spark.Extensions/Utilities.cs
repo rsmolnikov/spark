4b10c36fbb54965abca0bb955ad0742e3b4db070
Roman Smolnikov <roman.lcf@gmail.com>
2010-09-03 22:37:48 +0400

    code cleanup.

--- src/Spark.Extensions/Utilities.cs
+++ src/Spark.Extensions/Utilities.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Web.Configuration;
 using System.Web;
 using Spark.Parser.Markup;
 using System.Linq;
 using Spark.Parser.Code;
+using Spark.Compiler;
 
 namespace Spark.Extensions
 {
@@ -13,59 +13,16 @@ namespace Spark.Extensions
     {
         internal static AttributeNode AddMethodCallingToAttributeValue(AttributeNode node, string method)
         {
-            AttributeNode new_node = new AttributeNode(node.Name, String.Format(method, node.Value));
-
             Snippets snippets = new Snippets();
+            snippets.Add(new Snippet(){Value= method+"(H("});
+            snippets.AddRange(AttributeNodeExtensions.AsCodeInverted(node));
+            snippets.Add(new Snippet(){Value= "))"});
 
-            Snippet beginMethod = new Snippet();
-            beginMethod.Value = method+"(";
-            snippets.Add(beginMethod);
-
-            bool needPlus = false;
-            foreach (Node child in node.Nodes)
-            {
-                if (needPlus)
-                {
-                    Snippet snippet = new Snippet();
-                    snippet.Value = "+";
-                    snippets.Add(snippet);
-                }
-
-                Type typeOfChildNode=child.GetType();
-                switch (typeOfChildNode.Name)
-                {
-                    case ("TextNode"):
-                        Snippet snippet=new Snippet();
-                        snippet.Value+=String.Format(Constants.STRINGWRAPPER,((TextNode)child).Text);
-                        snippets.Add(snippet);
-                        break;
-                    case ("ExpressionNode"):
-                        Snippet hWrapperBegin = new Snippet();
-                        hWrapperBegin.Value = "H(";
-                        snippets.Add(hWrapperBegin);
-                        snippets.AddRange(((ExpressionNode)child).Code);
-                        Snippet hWrapperEnd = new Snippet();
-                        hWrapperEnd.Value = ")";
-                        snippets.Add(hWrapperEnd);
-                        break;
-
-                }
-                needPlus = true;
-            }
-            if (node.Nodes.Count == 0)
-            {
-                Snippet empty = new Snippet();
-                empty.Value = String.Format(Constants.STRINGWRAPPER,"");
-                snippets.Add(empty);
-            }
-            Snippet endMethod = new Snippet();
-            endMethod.Value = ")";
-
-            snippets.Add(endMethod);
-
-            new_node.Nodes[0] = new ExpressionNode(snippets);
-           
-            return new_node;
+            ExpressionBuilder builder = new ExpressionBuilder();
+            builder.AppendExpression(snippets);
+            List<Node> listNodes=new List<Node>();
+            listNodes.Add(new ExpressionNode(snippets));
+            return new AttributeNode(node.Name, listNodes);
         }
 
         internal static void AddApplyPathModifier(IList<AttributeNode> attributes, string name)
