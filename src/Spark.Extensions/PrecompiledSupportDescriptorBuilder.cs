93ac54bc728209366eaceb389d6a5376a69bef43
Roman Smolnikov <roman.lcf@gmail.com>
2010-11-16 22:46:06 +0300

    now views not needed after precompilation

---
+++ src/Spark.Extensions/PrecompiledSupportDescriptorBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Spark.Compiler;
+using Spark.Compiler.NodeVisitors;
+using Spark.Parser;
+using Spark.Parser.Syntax;
+using Spark.Web.Mvc.Descriptors;
+using Spark.Web.Mvc;
+
+namespace Spark.Extensions
+{
+    public class PrecompiledSupportDescriptorBuilder : DefaultDescriptorBuilder
+    {
+        private ISparkViewEngine _engine;
+        public IList<SparkViewDescriptor> Descriptors { get; set; }
+
+        public PrecompiledSupportDescriptorBuilder():base()  {}
+
+        public PrecompiledSupportDescriptorBuilder(ISparkViewEngine engine) : base(engine){ }
+
+        public PrecompiledSupportDescriptorBuilder(ISparkViewEngine engine, IList<SparkViewDescriptor> descriptors)
+            : base(engine) 
+        {
+            Descriptors = descriptors;
+        }
+
+        public override SparkViewDescriptor BuildDescriptor(BuildDescriptorParams buildDescriptorParams, ICollection<string> searchedLocations)
+        {
+            if (Descriptors != null)
+            {
+                var viewlocations = PotentialViewLocations(buildDescriptorParams.ControllerName,
+                             buildDescriptorParams.ViewName,
+                             buildDescriptorParams.Extra);
+                return Descriptors.FirstOrDefault(x => (buildDescriptorParams.FindDefaultMaster ? true : x.Templates.Count == 1) && x.Templates.Any(y => viewlocations.ToList().Contains(y)));
+            }
+            else return base.BuildDescriptor(buildDescriptorParams, searchedLocations);
+        }
+    }
+}
